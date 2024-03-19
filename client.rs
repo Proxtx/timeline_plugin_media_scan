b54c1f304c9a19f869632d5ee155b951a8ffc0bc
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-03-19 18:05:34 +0100

    create separate client file

---
+++ client.rs
@@ -0,0 +1,12 @@
+pub struct Plugin {}
+
+impl crate::Plugin for Plugin {
+    async fn new(
+        _data: crate::plugin_manager::PluginData,
+    ) -> Self
+    where
+        Self: Sized,
+    {
+        Plugin {}
+    }
+}
