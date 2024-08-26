06edf5efd671084f4e5b476268196ce7bfd64a88
Proxtx <gopro54122@gmail.com>
2024-08-26 23:56:54 +0200

    add a keygen project

---
+++ key_gen/src/main.rs
@@ -0,0 +1,15 @@
+use rsa::pkcs1v15::{SigningKey, VerifyingKey};
+use rsa::sha2::{Digest, Sha256};
+use rsa::signature::{Keypair, RandomizedSigner, SignatureEncoding, Verifier};
+use rsa::RsaPrivateKey;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+
+    let bits = 2048;
+    let private_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    println!(
+        "Private Key (between arrows): -->{}<--",
+        serde_json::to_string(&private_key).unwrap()
+    );
+}
