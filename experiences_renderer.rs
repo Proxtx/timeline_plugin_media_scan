fa5be7da41061a92b11acfaab261207d2af0fa5b
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-08-31 18:46:12 +0200

    random red to indicate loaded image

--- experiences_renderer.rs
+++ experiences_renderer.rs
@@ -1,6 +1,7 @@
-use raqote::{DrawTarget, Source, SolidSource, DrawOptions};
+use raqote::{DrawTarget, Source, SolidSource, DrawOptions, Image};
 use std::pin::Pin;
 use shared::timeline::types::api::CompressedEvent;
+use rand::Rng;
 
 pub struct PluginRenderer {}
 
@@ -9,10 +10,12 @@ impl crate::renderer::PluginRenderer for PluginRenderer {
         PluginRenderer {}
     }
 
-    fn render(&self, target: &mut DrawTarget, _event: &CompressedEvent) -> Pin<Box<dyn std::future::Future<Output = Result<(), String>> + Send>> {
-        Box::pin(async {
-            target.fill_rect(0., 0., 50., 50., &Source::Solid(SolidSource::from_unpremultiplied_argb(255, 0, 180, 0)), &DrawOptions::new());
-            Ok(())
+    fn render(&self, dimensions: (i32, i32), _event: &CompressedEvent) -> Pin<Box<dyn std::future::Future<Output = Result<Vec<u32>, String>> + Send>> {
+        Box::pin(async move {
+            let mut rng = rand::thread_rng();
+            let mut target = DrawTarget::new(dimensions.0, dimensions.1);
+            target.fill_rect(0., 0., dimensions.0 as f32, dimensions.1 as f32, &Source::Solid(SolidSource::from_unpremultiplied_argb(255, rng.gen(), 0, 0)), &DrawOptions::new());
+            Ok(target.into_vec())
         })
     }
 }
